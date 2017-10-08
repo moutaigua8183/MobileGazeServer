615c216958193fd4b5af081a8ee977e44bb093c0
moutaigua8183 <moutaigua8183@gmail.com>
2017-10-08 18:53:52 -0400

    Update analyze.js

--- analyze.js
+++ analyze.js
@@ -1,7 +1,19 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
